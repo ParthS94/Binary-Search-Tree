3011593f0b9a40571688e22011578bbcd2148606
parthmshroff@gmail.com <41526125+ParthS94@users.noreply.github.com>
2020-05-25 20:34:26 -0500

    Basic setup

---
+++ BST.cpp
@@ -0,0 +1,9 @@
+//
+// Created by parth on 5/25/2020.
+//
+
+#include "BST.h"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
---
+++ BST.h
@@ -0,0 +1,21 @@
+//
+// Created by parth on 5/25/2020.
+//
+
+#ifndef BINARYSEARCHTREE_BST_H
+#define BINARYSEARCHTREE_BST_H
+
+struct node
+{
+    int key_value;
+    struct node *p_left;
+    struct node *p_right;
+};
+
+class BST {
+public:
+    node* root
+};
+
+
+#endif //BINARYSEARCHTREE_BST_H
